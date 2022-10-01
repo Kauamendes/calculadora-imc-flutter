bb0762dc3112b393e0ff233f3b8e797b051552f2
Kauã Mendes <kaua.mendes@attornatus.com.br>
2022-09-30 21:26:58 -0300

    finalizando

--- lib/main.dart
+++ lib/main.dart
@@ -8,26 +8,69 @@ void main() {
 class MeuContador extends StatefulWidget {
   const MeuContador({super.key});
 
+
   @override
   State<MeuContador> createState() => _MeuContadorState();
 }
 
 class _MeuContadorState extends State<MeuContador> {
 
+TextEditingController pesoControler = TextEditingController();
+TextEditingController alturaControler = TextEditingController();
+
+GlobalKey<FormState> formController = GlobalKey<FormState>();
+
+String msg = 'Informe seus dados!';
+
+limparCampos() {
+    pesoControler.text = "";
+    alturaControler.text = "";
+    setState(() {
+      msg = "Informe os seus dados!";
+      formController = GlobalKey<FormState>();
+    });
+  }
+
+  calcularIMC() {
+    setState(() {
+      double peso = double.parse(pesoControler.text);
+      double altura = double.parse(alturaControler.text);
+      double imc = peso / (altura * altura);
+      String imcTxt = imc.toStringAsPrecision(4);
+      if(imc <18.6) {
+        msg = "Abaixo do peso imc: ($imcTxt)";
+      } else if(imc > 18.6 && imc < 24.9) {
+        msg = "Peso ideal imc: ($imcTxt)";
+      } else {
+        msg = "Acima do peso imc: ($imcTxt)";
+      }
+     });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: paginaPrincipal(),
+      home: Scaffold(
+      appBar: Util().criaAppBar(limparCampos),
+      body: Container(
+        color: Colors.white,
+        child: Form(
+          key: formController,
+          child: Column(
+          children: [
+            const SizedBox(height: 20),
+            Util().criaIconeGrande(),
+            Util().criaInputText(TextInputType.number, "Peso(Kg)", pesoControler, "Informe seu peso"),
+            Util().criaInputText(TextInputType.number, "Altura", alturaControler, "Informe sua altura"),
+            Util().criaBotao("Calcular", 30, Colors.white, calcularIMC, Colors.blue, formController),
+            Util().criaTexto(msg, 30, Colors.blue, false),
+          ],
+        ),
+       ),
+      ),
+    ),
     );
   }
 
-  paginaPrincipal() {
-    return Scaffold(
-      appBar: Util().criaAppBar(),
-      body: Container(
-        
-      ),
-    );
-  }
 }
--- lib/util.dart
+++ lib/util.dart
@@ -3,53 +3,89 @@ import 'package:flutter/widgets.dart';
 
 class Util {
 
-  criaTexto(conteudo,tamanho,cor) {
+  criaTexto(conteudo,tamanho,cor, negrito) {
     return Text(
       conteudo,
       style: TextStyle(
         fontSize: tamanho,
-        fontWeight: FontWeight.bold,
+        fontWeight: negrito ? FontWeight.bold : FontWeight.normal,
         color: cor,
       ),
     );
   }
 
-  criaBotao(conteudoTxt, tamanhoTxt,corTxt,acao, corFundoBtn) {
-   return TextButton(
-    style: TextButton.styleFrom(
-      backgroundColor: corFundoBtn,
-      fixedSize: const Size(100, 100),
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
-    ),
-      onPressed: acao,
-      child: criaTexto(conteudoTxt, tamanhoTxt, corTxt),
-    );
+  criaBotao(conteudoTxt, tamanhoTxt,corTxt,acao, corFundoBtn, controladorFormulario) {
+   return Row(
+      children: [
+        Expanded( 
+          flex: 1,
+          child: Container(
+            padding: const EdgeInsets.fromLTRB(5, 10, 5, 10),
+            height: 70,
+            child:  ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: corFundoBtn,
+              ),
+              onPressed: () {
+                if (controladorFormulario.currentState!.validate()) {
+                  acao();
+                }
+              },
+              child: criaTexto(conteudoTxt, tamanhoTxt, corTxt, false),
+            ),
+          ),
+      )
+    ],
+   ); 
   }
 
-  criaAppBar() {
+  criaAppBar(funcao) {
     return AppBar(
       backgroundColor: Colors.blue,
-      title: criaTexto('Calculadora IMC', 30.0, Colors.white),
+      title: criaTexto('Calculadora IMC', 30.0, Colors.white,false),
       centerTitle: true,
-      actions: [
+      actions: <Widget> [
         Row(
           children: <Widget> [
-            criaIconButton(Icons.refresh, null, Colors.white)
+            IconButton(
+              onPressed: funcao,
+              icon: const Icon(Icons.refresh),
+              )
           ],
-        )
+        ),
       ],
     );
   }
 
-  criaIconButton(icon, acao, corIcon) {
-    return Row(
-          children: <Widget> [
-            IconButton(
-              onPressed: acao,
-              icon: Icon(icon, color: corIcon),
-              )
-          ],
-        );
+  criaIconeGrande() {
+    return const Icon(
+      Icons.person_outline,
+      size: 120.0,
+      color: Colors.blue,
+    );
+  }
+
+  criaInputText(tipoTeclado, textoEtiqueta, controlador, msgValidacao) {
+    return TextFormField(
+      keyboardType: tipoTeclado,
+      decoration: InputDecoration(
+        labelText: textoEtiqueta,
+        labelStyle: const TextStyle(
+          color: Colors.blue,
+        ),
+      ),
+      textAlign: TextAlign.center,
+      style: const TextStyle(
+        color: Colors.blue,
+        fontSize: 25.0,
+      ),
+      controller: controlador,
+      validator: (value) {
+        if (value!.isEmpty) {
+          return msgValidacao;
+        }
+      }
+    );
   }
 
 }
